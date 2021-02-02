6458cbb7c38e3487adf6519af0939e77baf16c05
Joel Berenguer Martinez <joel7397@linkiafp.es>
2021-02-02 21:02:08 +0100

    Updates

---
+++ precache-manifest.15ad6316fd1dbad70a13c10f1233bb95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd185b7dbdac9713205da98fd90a6ab",
+    "url": "/JoelBM-creator/porfolio-carla/index.html"
+  },
+  {
+    "revision": "d0e14c1699b9a145afae",
+    "url": "/JoelBM-creator/porfolio-carla/static/js/2.84de95b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JoelBM-creator/porfolio-carla/static/js/2.84de95b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34704df9e9737e77fa26",
+    "url": "/JoelBM-creator/porfolio-carla/static/js/main.f69ef5d6.chunk.js"
+  },
+  {
+    "revision": "6d69db0a27da879776fb",
+    "url": "/JoelBM-creator/porfolio-carla/static/js/runtime-main.2daa6a8a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b07117614e85dbaa8abd2c2d55709fdd.js"
+  "/JoelBM-creator/porfolio-carla/precache-manifest.15ad6316fd1dbad70a13c10f1233bb95.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/JoelBM-creator/porfolio-carla/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
